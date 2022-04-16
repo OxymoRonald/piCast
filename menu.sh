e62975ea1fac2fd24e164d9de9923f6b75310a20
Oxymoronald <rwiersema@gmail.com>
2022-04-16 23:20:53 +0000

    whiptail test

--- menu.sh
+++ menu.sh
@@ -1,1 +1,19 @@
-ls -a
+#!/bin/bash
+
+# Clear the screen.
+clear
+
+# -------------------------------------------------- #
+#     Import external script files from ./portal/    #
+# -------------------------------------------------- #
+echo "Importing files"
+for i in ./portal/*;
+  do 
+  	echo "Importing $i"
+  	source $i
+done
+
+# -------------------------------------------------- #
+# 	   Display welcome screen. (Start the menu)      #
+# -------------------------------------------------- #
+welcome
---
+++ portal/general.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This function displays the welcome screen.
+function welcome {
+    whiptail --title "PI Client Management" \
+             --msgbox "\n
+                    Welcome to the PI Management Menu
+                    This menu system is keyboard only
+
+                      Hitting <Cancel> at any time
+                       will exit the menu system
+
+                       Press <Enter> to continue "\
+             --ok-button "OK" 16 78
+    # This function will take us to the main menu.
+    #home_menu
+}
